b83cef95d55a82487c1374eaadf9df89fd560653
Kevin Saito <yum02.skullcandy@gmail.com>
2015-12-10 15:35:33 +0900

    add base stage and stage 0

--- kagefumi/Assets/Scripts/Game/StageManager.cs
+++ kagefumi/Assets/Scripts/Game/StageManager.cs
@@ -4,6 +4,14 @@ using System.Collections.Generic;
 
 public class StageManager : GameMonoBehaviour
 {
+	private GameObject stageGameObject;
+
+	[SerializeField]
+	private Transform baseStagePrefab;
+	private Transform baseStageTransform;
+
+	private const string STAGE_PREFAB_PATH = "Prefabs/Stages/";
+
 	public void Init()
 	{
 		SetStage();
@@ -11,6 +19,35 @@ public class StageManager : GameMonoBehaviour
 
 	private void SetStage()
 	{
+		int stageId = 0;
 
+#if UNITY_EDITOR
+		// For Debug
+		stageId = 0;
+#endif
+
+		InstantiateStage(stageId);
+	}
+
+	private void InstantiateStage(int id)
+	{
+		InstantiateBaseStage();
+
+		if (stageGameObject != null)
+		{
+			Destroy(stageGameObject);
+		}
+
+		stageGameObject = Instantiate(Resources.Load<GameObject>(STAGE_PREFAB_PATH + id));
+		stageGameObject.transform.SetParent(baseStageTransform);
+	}
+
+	private void InstantiateBaseStage()
+	{
+		if (baseStageTransform == null)
+		{
+			baseStageTransform = Instantiate(baseStagePrefab).transform;
+			baseStageTransform.transform.SetParent(transform);
+		}
 	}
 }
