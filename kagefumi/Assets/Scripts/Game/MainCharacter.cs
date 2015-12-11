6ed3a5e409f21be737379e2338f6f18b2070c0e9
Kevin Saito <yum02.skullcandy@gmail.com>
2015-12-11 22:19:11 +0900

    add arrow key for debug

--- kagefumi/Assets/Scripts/Game/MainCharacter.cs
+++ kagefumi/Assets/Scripts/Game/MainCharacter.cs
@@ -30,25 +30,12 @@ public class MainCharacter : GameMonoBehaviour
 	{
 		Vector3 moveDirection = Vector3.zero;
 
-		if(Input.GetKey(KeyCode.W))
-		{
-			moveDirection.z = 1;
-		}
-
-		if(Input.GetKey(KeyCode.A))
-		{
-			moveDirection.x = -1;
-		}
-
-		if(Input.GetKey(KeyCode.S))
-		{
-			moveDirection.z = -1;
-		}
-
-		if(Input.GetKey(KeyCode.D))
-		{
-			moveDirection.x = 1;
-		}
+#if UNITY_EDITOR
+		if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {moveDirection.z = 1;}
+		if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {moveDirection.x = -1;}
+		if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {moveDirection.z = -1;}
+		if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {moveDirection.x = 1;}
+#endif
 
 		if (moveDirection != Vector3.zero)
 		{
