2f50c45af50e57bc2c86b9c3f6b3a3d0c7d696da
Kevin Saito <yum02.skullcandy@gmail.com>
2015-12-13 02:52:46 +0900

    split prefab for game view ui parts

--- kagefumi/Assets/Scripts/Game/GameUIManager.cs
+++ kagefumi/Assets/Scripts/Game/GameUIManager.cs
@@ -6,6 +6,9 @@ using System.Collections.Generic;
 
 public class GameUIManager : GameMonoBehaviour
 {
+	[SerializeField]
+	private List<GameObject> uiPartsPrefabs;
+
 	private TapDetector tapDetector_;
 	private TapDetector tapDetector
 	{
@@ -19,17 +22,43 @@ public class GameUIManager : GameMonoBehaviour
 		}
 	}
 
-	[SerializeField]
-	private Joystick joystick;
+	private Joystick joystick_;
+	private Joystick joystick
+	{
+		get
+		{
+			if (joystick_ == null)
+			{
+				joystick_ = GetComponentInChildren<Joystick>();
+			}
+			return joystick_;
+		}
+	}
 
 	public void Init(System.Action onDoubleTap)
 	{
+		InitializeUIParts();
+
 		tapDetector.onDoubleTap = onDoubleTap;
 		tapDetector.onUp = OnUp;
 		tapDetector.onDrag = OnDrag;
 
 		HideJoystick();
 		joystick.CreateVirtualAxes();
+	}
+
+	private void InitializeUIParts()
+	{
+		if (transform.childCount == 0)
+		{
+			foreach (GameObject prefab in uiPartsPrefabs)
+			{
+				RectTransform partsTransfrom = Instantiate(prefab).transform as RectTransform;
+				Vector3 position = partsTransfrom.anchoredPosition;
+				partsTransfrom.SetParent(transform);
+				partsTransfrom.anchoredPosition = position;
+			}
+		}
 	}
 
 	public void ShowJoystick(Vector3 position)
