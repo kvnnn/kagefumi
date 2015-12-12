7c1f9d6ef341c9bec98e3a01fd391ec55565edad
Kevin Saito <yum02.skullcandy@gmail.com>
2015-12-13 01:40:27 +0900

    use playerprefs for user stage id

---
+++ kagefumi/Assets/Scripts/User.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class User
+{
+	public const string STAGE_ID_KEY = "user_stage_id";
+	public const int DEFAULT_STAGE_ID = 1;
+
+	public static int stageId
+	{
+		get
+		{
+			if (PlayerPrefs.HasKey(STAGE_ID_KEY))
+			{
+				return PlayerPrefs.GetInt(STAGE_ID_KEY);
+			}
+			else
+			{
+				return DEFAULT_STAGE_ID;
+			}
+		}
+
+		set
+		{
+			PlayerPrefs.SetInt(STAGE_ID_KEY, value);
+			PlayerPrefs.Save();
+		}
+	}
+}
