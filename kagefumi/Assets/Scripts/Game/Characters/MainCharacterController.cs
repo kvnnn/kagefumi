b0f9b8c39841da869b6aed8fd4cd5b1a4984c1fb
Kevin Saito <kevin.ysaito@gmail.com>
2015-12-30 02:16:09 +0900

    detect cube collision

--- kagefumi/Assets/Scripts/Game/Characters/MainCharacterController.cs
+++ kagefumi/Assets/Scripts/Game/Characters/MainCharacterController.cs
@@ -14,6 +14,12 @@ public class MainCharacterController : GameMonoBehaviour
 		}
 	}
 
+	private CubeObject jumpTarget;
+	private float collisionLastTime;
+	private float collisionTotalTime;
+	private const float JUMP_DECISION_INTERVAL_SEC = 0.1f;
+	private const float JUMP_DECISION_TOTAL_SEC = 0.25f;
+
 	public System.Action<Vector3> onUpdate;
 
 	private CharacterController controller
@@ -75,4 +81,50 @@ public class MainCharacterController : GameMonoBehaviour
 
 		controller.SimpleMove(forward);
 	}
+
+#region ClimbCube
+	private bool IsClimbableCube(CubeObject cube)
+	{
+		return cube != null && cube.isClimbable && transform.position.y < cube.TopPositionY();
+	}
+
+	private void Climb(CubeObject cube)
+	{
+		if (jumpTarget == null || jumpTarget != cube)
+		{
+			jumpTarget = cube;
+			collisionTotalTime = 0f;
+			collisionLastTime = Time.time;
+		}
+
+		float collisionInterval = Time.time - collisionLastTime;
+		collisionLastTime = Time.time;
+
+		if (collisionInterval > JUMP_DECISION_INTERVAL_SEC)
+		{
+			collisionTotalTime = 0f;
+		}
+		else
+		{
+			collisionTotalTime += collisionInterval;
+		}
+
+		if (collisionTotalTime > JUMP_DECISION_TOTAL_SEC)
+		{
+			UnityEngine.Debug.LogError("hoge");
+		}
+	}
+#endregion
+
+#region Event
+	private void OnControllerColliderHit(ControllerColliderHit hit)
+	{
+		CubeObject cube = hit.collider.transform.parent.GetComponent<CubeObject>();
+
+		if (IsClimbableCube(cube))
+		{
+			Climb(cube);
+		}
+	}
+#endregion
 }
--- kagefumi/Assets/Scripts/Game/Stages/CubeObject.cs
+++ kagefumi/Assets/Scripts/Game/Stages/CubeObject.cs
@@ -5,9 +5,29 @@ using System.Collections.Generic;
 
 public class CubeObject : BaseObject
 {
+	private const float CLIMBABLE_SCALE = 1f;
+
+	public bool isClimbable
+	{
+		get
+		{
+			return transform.localScale.y <= CLIMBABLE_SCALE;
+		}
+	}
+
 	public override Vector3 GetOutPosition()
 	{
 		return base.GetOutPosition();
 		// return new Vector3(transform.position.x, transform.position.y + transform.localScale.y/2, transform.position.z);
+	}
+
+	public bool IsSide(Vector3 position)
+	{
+		return position.y < transform.position.y;
+	}
+
+	public float TopPositionY()
+	{
+		return transform.position.y + transform.localScale.y/2;
 	}
 }
